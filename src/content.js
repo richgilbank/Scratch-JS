31a473074288f4c789d62ee408ccdddf3b407eda
Rich Gilbank <richgilbank@gmail.com>
2014-04-29 14:22:19 -0400

    Fix exception handling

--- src/content.js
+++ src/content.js
@@ -1,10 +1,10 @@
-var traceurWebPageTranscoder = new traceur.WebPageTranscoder(document.location.href);
-var traceurScriptName = traceurWebPageTranscoder.nextInlineScriptName_();
-
 traceur.options.experimental = true;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   try {
+    var traceurWebPageTranscoder = new traceur.WebPageTranscoder(document.location.href);
+    var traceurScriptName = traceurWebPageTranscoder.nextInlineScriptName_();
+
     traceurWebPageTranscoder.addFileFromScriptElement(null, traceurScriptName, request.content);
   }
   catch(e){
