352d0c2aacb6d9488c92737030a992eb77b69376
Rich Gilbank <richgilbank@gmail.com>
2015-02-15 14:47:21 -0500

    Add build system

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,14 +1,54 @@
 var gulp = require('gulp');
 var tinylr = require('tiny-lr');
+var usemin = require('gulp-usemin');
+var del = require('del');
+var zip = require('gulp-zip');
+
+var FILES = {
+  copy: [
+    '**/*',
+    '!{panel,panel/**}',
+    '!{node_modules,node_modules/**}',
+    '!package.json',
+    '!gulpfile.js'
+  ],
+  watch: ['panel/*.{js,css,html}'],
+  panel: 'panel/repl.html',
+  dist: '../dist/',
+  distAll: '../dist/**',
+  zip: 'ES6 Repl.zip',
+  root: '../'
+}
 
 gulp.task('dev', function () {
   var lr = tinylr();
   lr.listen(35729);
-  gulp.watch(['**/*.{js,css,html}'], function (evt) {
+  gulp.watch(FILES.watch, function (evt) {
     lr.changed({
       body: {
         files: [evt.path]
       }
     });
   });
+});
+
+gulp.task('copy', function() {
+  return gulp.src(FILES.copy)
+    .pipe(gulp.dest(FILES.dist));
+});
+
+gulp.task('usemin', ['clean'], function() {
+  return gulp.src(FILES.panel)
+    .pipe(usemin())
+    .pipe(gulp.dest(FILES.dist));
+});
+
+gulp.task('clean', function(cb) {
+  del([FILES.distAll], {force: true}, cb);
+});
+
+gulp.task('zip', ['usemin', 'copy'], function() {
+  return gulp.src(FILES.distAll)
+    .pipe(zip(FILES.zip))
+    .pipe(gulp.dest(FILES.root));
 });
