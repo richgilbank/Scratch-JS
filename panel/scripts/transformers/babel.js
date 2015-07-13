df27c89c17db7638e8b4e0175ca73877559d6527
allenyang <allenyang@appier.com>
2015-07-13 16:19:30 +0800

    add syntax check before transform on babel

--- panel/scripts/transformers/babel.js
+++ panel/scripts/transformers/babel.js
@@ -13,8 +13,25 @@ function Babel() {
 Babel.prototype = Object.create(Transformer.prototype);
 Babel.prototype.constructor = Babel;
 
+Babel.prototype.beforeTransform = function(){
+  bus.trigger('transformers:beforeTransform');
+}
 Babel.prototype.transform = function(input) {
-  return babel.transform(input, this.opts).code;
+  try{
+    var ret = babel.transform(input, this.opts).code;
+    return ret
+  }catch(err){
+    if(err.name === "SyntaxError"){
+      var message = "<pre>"+err.toString()+"</pre>";
+      bus.trigger("transformers:error",{
+        line: err.loc.line - 1,
+        column: err.loc.column
+      },message);
+    }else{
+      throw err;
+    }
+    return null;
+  }
 }
 
 Babel.prototype.getVersion = function() {
