bf50e30276a3e85408e91724113d65a7413c4817
Andres Garcia <oagarciar@gmail.com>
2016-02-25 18:30:10 -0500

    Add autocompletion

--- panel/scripts/repl.js
+++ panel/scripts/repl.js
@@ -42,7 +42,7 @@ Repl.prototype.onDomReady = function() {
     lineNumbers: true,
     matchBrackets: true,
     continueComments: "Enter",
-    extraKeys: {"Ctrl-Q": "toggleComment"},
+    extraKeys: {"Ctrl-Q": "toggleComment", "Ctrl-Space": "autocomplete"},
     tabSize: this.settings.data.tabSize || 2,
     indentUnit: this.settings.data.tabSize || 2,
     indentWithTabs: this.settings.data.indentWithTabs || false,
