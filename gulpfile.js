543e1c720898b724e4bb34129d4304ed511643ea
Rich Gilbank <richgilbank@gmail.com>
2019-02-28 09:57:57 -0500

    building again - update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ var gulp   = require('gulp'),
     usemin = require('gulp-usemin'),
     del    = require('del'),
     zip    = require('gulp-zip'),
-    mfst   = require('./manifest.json')
-    stylus = require('gulp-stylus');
+    mfst   = require('./manifest.json'),
+    stylus = require('gulp-stylus'),
+    debug  = require('gulp-debug');
+
 
 var FILES = {
   copy: [
@@ -27,10 +29,16 @@ var FILES = {
   root: './'
 }
 
-gulp.task('dev', ['stylus'], function () {
+gulp.task('stylus', function() {
+  return gulp.src(FILES.stylus)
+    .pipe(stylus())
+    .pipe(gulp.dest(FILES.stylusRoot));
+});
+
+gulp.task('dev', gulp.series('stylus', function () {
   var lr = tinylr();
   lr.listen(35729);
-  gulp.watch(FILES.stylusGlob, ['stylus']);
+  gulp.watch(FILES.stylusGlob, gulp.series('stylus'));
   gulp.watch(FILES.watch, function (evt) {
     lr.changed({
       body: {
@@ -38,34 +46,30 @@ gulp.task('dev', ['stylus'], function () {
       }
     });
   });
-});
-
-gulp.task('stylus', function() {
-  return gulp.src(FILES.stylus)
-    .pipe(stylus())
-    .pipe(gulp.dest(FILES.stylusRoot));
-});
-
-gulp.task('copy', ['clean'], function() {
-  return gulp.src(FILES.copy)
-    .pipe(gulp.dest(FILES.dist));
-});
-
-gulp.task('usemin', ['clean'], function() {
-  return gulp.src(FILES.panel)
-    .pipe(usemin())
-    .pipe(gulp.dest(FILES.distPanel));
-});
+}));
 
 gulp.task('clean', function() {
   return del([FILES.distAll, FILES.zip], {force: true});
 });
 
-gulp.task('zip', ['stylus', 'clean', 'usemin', 'copy'], function() {
+gulp.task('copy', function() {
+  return gulp.src(FILES.copy)
+    .pipe(gulp.dest(FILES.dist));
+});
+
+gulp.task('usemin', function() {
+  return gulp.src(FILES.panel)
+    .pipe(debug({title: 'usemin:'}))
+    .pipe(usemin())
+    .pipe(debug({title: 'dest:'}))
+    .pipe(gulp.dest(FILES.distPanel));
+});
+
+gulp.task('zip', gulp.series('stylus', 'clean', 'usemin', 'copy', function() {
   return gulp.src(FILES.distAll)
     .pipe(zip(FILES.zip))
     .pipe(gulp.dest(FILES.root));
-});
+}));
 
-gulp.task('default', ['dev']);
-gulp.task('build', ['zip']);
+gulp.task('default', gulp.series('dev'));
+gulp.task('build', gulp.series('zip'));
